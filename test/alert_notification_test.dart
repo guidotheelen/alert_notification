f092d8e6cae5445db14ff551fff693fe9b4782cc
Guido Theelen <guido@kabisa.nl>
2022-08-17 08:18:39 +0200

    test: show components

--- test/alert_notification_test.dart
+++ test/alert_notification_test.dart
@@ -1,5 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:alert_notification/alert_notification.dart';
 
-void main() {}
+void main() {
+  const title = 'AlertNotification';
+  const body = 'You should know this.';
+  const Widget infoNotification = MaterialApp(
+    home: Material(
+      child: AlertNotification(
+        title: title,
+        body: body,
+        type: AlertNotificationType.info,
+      ),
+    ),
+  );
+
+  testWidgets('shows components', (tester) async {
+    await tester.pumpWidget(infoNotification);
+
+    expect(find.byType(Icon), findsOneWidget);
+    expect(find.text(title), findsOneWidget);
+    expect(find.text(body), findsOneWidget);
+  });
+}
